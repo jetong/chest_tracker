ab705faa4c2fe43327cc10a4a5ea832d200b0baf
jetong <jose.e.tong@gmail.com>
2018-02-21 00:14:15 +0000

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "My First Javascript<br />";
+}
+myFunction();
+
+function display(){
+	document.getElementById("demo").innerHTML += "button pressed<br />";
+}
+

0260f7367d8e01b00cc6ace3085cb23f456113c8
Ubuntu <ubuntu@ip-172-31-29-3.us-east-2.compute.internal>
2018-02-21 03:46:15 +0000

    fixed scope issues of variables used within callbacks

--- index.js
+++ index.js
@@ -5,32 +5,32 @@ var getJSON = require('get-json');
 
 app.use('/', express.static('public'));
 
-app.use('/handleForm', (req, res) => {
-  var id, chest, key;
 
-  fs.readFile('private/.api_key', function (err, data) {
+app.use('/handleForm', (req, res) => {
+  fs.readFile('private/.api_key', function (err, key) {
     if (err) {
       console.log(err);
     }
-    key = data;
+    getId(key);
+  });
+
+  function getId(key) {
+    var url_id = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/mycon/?api_key=" + key;
+    getJSON(url_id, function(error, response) {
+        var id = String(response.id);
+  			getChests(id,key);
+    });
   }
 
-  var url_id = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/mycon/?api_key=" + data;
-  getJSON(url_id, function(error, response) {
-    id = String(response.id);
-    //res.write(JSON.stringify(response));
-  });
-
-  var url_chests = "https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/" + id + "?api_key=" + data;
-  getJSON(url_chests, function(error, response) {
-    //	chest = String(response.chestGranted);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    //res.write(id);
-  res.end();
+  function getChests(id,key) {
+    var url_chests = "https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/" + id + "?api_key=" + key;
+    getJSON(url_chests, function(error, response) {
+      //	chest = String(response.chestGranted);
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(JSON.stringify(response));
+      res.end();
     });
-  });
-  //res.writeHead(200, { 'Content-Type': 'text/html' });
-  //res.write(chest);
+  }
 });
 
 app.listen(3000, () => {

17dddca8ac5f8916f059face15e869f0262c0f11
jetong <jose.e.tong@gmail.com>
2018-02-21 21:21:13 +0000

    add files: run.go - equivalent to run.js.  considering using goroutines with mongodb user.js - set up mongo schema for use in run.js

---
+++ user.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/lol');
+
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+            username: {type: String, required: true, unique: true},    
+                  id: String,
+                days: String,
+               hours: String,
+             minutes: String,
+           timestamp: String,
+        total_chests: String,
+    available_chests: String 
+});
+
+// export this schema as a User class so that other files can create users with this schema.
+module.exports = mongoose.model('User', userSchema);

515c60b7a0600c5260ace25ee033ec50573b0957
jetong <jose.e.tong@gmail.com>
2018-02-27 23:05:10 +0000

    index.html: Add error messages for input validation as hidden fields validateForm.js: Show messages on blur when invalid run.go: Removed.  No longer implementing backend with Go. styles.css: Add error-text class

--- public/js/validateForm.js
+++ public/js/validateForm.js
@@ -1,4 +1,18 @@
+$("input[name='username']").blur(function() {
+  var usernameField = $("input[name='username']");
+  var username = usernameField.val();
+  if(username.length < 3) {
+    $("#name-length").show();
+    $("#error-message").show();
+  } else {
+    $("#name-length").hide();
+    $("#error-message").hide();
+  }
+}
+
 function validateForm() {
   var name = $("#name-input").val();
-  alert(name);
+  if (name == "") {
+    $("#errorMessages").append("<li>Please enter name</li>");
+  }
 }

c04c90804c63063af5100cf95a538f1eeabffe34
jetong <jose.e.tong@gmail.com>
2018-03-05 06:29:59 +0000

    remove test links

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  document.getElementById("demo").innerHTML = "My First Javascript<br />";
-}
-myFunction();
-
-function display(){
-	document.getElementById("demo").innerHTML += "button pressed<br />";
-}
-
